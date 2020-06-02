671f9e0749621e99455a15839e2b5ae633000006
silvercoin <silvercoin99.999@gmail.com>
2020-06-03 08:35:41 +0900

    node.js DB연결 셋팅

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,63 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/customers', (req, res) => {
-  res.send([
-    {
-      'id': 1,
-      'image': 'https://placeimg.com/64/64/1',
-      'name': '홍길동',
-      'birthday': '961125',
-      'gender': '남자',
-      'job': '대학생'
-    },
-    {
-      'id': 2,
-      'image': 'https://placeimg.com/64/64/2',
-      'name': '냥냥이',
-      'birthday': '961125',
-      'gender': '남자',
-      'job': '프로그래머'
-    },
-    {
-      'id': 3,
-      'image': 'https://placeimg.com/64/64/3',
-      'name': '멍멍이',
-      'birthday': '961125',
-      'gender': '남자',
-      'job': '대학생'
-    }
-  ]);
-});
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+  multipleStatements: true,
+  typeCast: function (field, next) {
+    if (field.type == 'VAR_STRING') {
+      return field.string();
+    }
+    return next();
+  }
+});
+connection.connect();
+
+app.get('/api/customers', (req, res) => {
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows, fields) => {
+      console.log(rows);
+      res.send(rows);
+    }
+  );
+});
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
+
+// res.send([
+  //   {
+  //     'id': 1,
+  //     'image': 'https://placeimg.com/64/64/1',
+  //     'name': '홍길동',
+  //     'birthday': '961125',
+  //     'gender': '남자',
+  //     'job': '대학생'
+  //   },
+  //   {
+  //     'id': 2,
+  //     'image': 'https://placeimg.com/64/64/2',
+  //     'name': '냥냥이',
+  //     'birthday': '961125',
+  //     'gender': '남자',
+  //     'job': '프로그래머'
+  //   },
+  //   {
+  //     'id': 3,
+  //     'image': 'https://placeimg.com/64/64/3',
+  //     'name': '멍멍이',
+  //     'birthday': '961125',
+  //     'gender': '남자',
+  //     'job': '대학생'
+  //   }
+  // ]);
